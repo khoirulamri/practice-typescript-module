c752ec4b49ff1b5f927bbbf79cfe308a3919a78d
Khoirul Amri <khoirulamri09@gmail.com>
2022-01-10 21:10:37 +0700

    fixing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,33 +1,17 @@
-import ExpressCache from './index';
-
 jest.mock('redis', () => jest.requireActual('redis-mock'));
 
 import redis from 'redis';
 
+import ExpressCache from './index';
+
 describe('class construction options', () => {
   test('can instantiate class with required options', async () => {
     const client = await redis.createClient();
-    console.log('save',await client.set('product-','test redis'));
-console.log(await client.keys('product*'));
+
     const expressCache = new ExpressCache({
-      redisClient: client
+      redisClient: client,
     });
 
     expect(expressCache).toEqual(expect.any(ExpressCache));
   });
 });
-
-// describe('error handler', () => {
-
-//   test('should throw error on read cache from redis', async () => {
-//     redis.
-//     const client = await redis.createClient();
-    
-
-//     const expressCache = new ExpressCache({
-//       redisClient: client
-//     });
-
-//     await client.disconnect();
-//   })
-// });
